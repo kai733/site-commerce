89b09b04748a43704b8e9a687913cf46133396dd
kai733 <kai.djepaxhia@gmail.com>
2025-01-17 18:54:55 +0100

    ajout logo

--- include/entete.php
+++ include/entete.php
@@ -1,7 +1,10 @@
 <header>
     <nav class="navbar">
         <div class="container">
-            <a href="index.php" class="logo">PixelPlay</a>
+            <a href="index.php" class="logo">
+                <img src="img/logo.png" alt="PixelPlay Logo" class="logo-img">
+                <span class="logo-text">PixelPlay</span>
+            </a>
             <ul class="nav-links">
                 <li><a href="index.php">Accueil</a></li>
                 <li><a href="catalogue.php">Catalogue</a></li>
